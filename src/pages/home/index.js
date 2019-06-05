1bd7ca48e3c9eb743a449cb1e02d9e5435942102
86driver <610410574@qq.com>
2019-06-05 22:49:31 +0800

    完成， 离学会react又进了一步

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -13,7 +13,7 @@ class Home extends PureComponent {
     return (
       <HomeWrapper>
         <HomeLeft>
-          <img className="banner-img" src="//upload.jianshu.io/admin_banners/web_images/4660/224da83c76e01d5deff07e163615921233af5c82.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt="" />
+          <img className="banner-img" src="http://upload.jianshu.io/admin_banners/web_images/4660/224da83c76e01d5deff07e163615921233af5c82.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt="" />
           <Topic></Topic>
           <List></List>
         </HomeLeft>
